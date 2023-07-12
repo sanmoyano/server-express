69bb4091efc4823fe64945c09cfd9f4743697e88
sanmoyano <smoyano.di@gmail.com>
2023-07-12 20:29:35 -0300

    updating

--- src/app.js
+++ src/app.js
@@ -2,39 +2,83 @@ const express = require('express')
 const app = express()
 const ProductManager = require('./productManager')
 const producManager = new ProductManager('products.json')
+const routes = require('./routes')
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('HOLA PRODUCTOS')
 })
 
-app.get('/productos', async (req,res) => {
+app.get('/api/products', async (req,res) => {
   const { limit } = req.query
-  console.log(`Buscando productos con ${limit}`)
   const products = await producManager.getProducts()
   
   let filteredProducts = products
 
   if(limit){
     filteredProducts = filteredProducts
-      .filter(p => p.title.includes(limit) || p.description.includes(limit))
+      .filter(p => p.thumbnail.includes(limit.toLowerCase()) || p.title.includes(limit.toLowerCase()) || p.description.includes(limit.toLowerCase()))
     res.send(filteredProducts)
   } else {
     res.send(products)
   }
 })
 
-app.get('/productos/:id', async (req, res) => {
+app.get('/api/products/:id', async (req, res) => {
   const { id } = req.params
   const productId = parseInt(id)
   const product = await producManager.getProductById(productId)
 
-  if (product) {
-    res.send(product);
-  } else {
-    res.send('El producto no existe');
+  if(!product){
+    res.sendStatus(404)
+    return
   }
+
+  res.send(product)
+})
+
+app.post('/api/products', async (req, res) => {
+  const { body } = req
+  const product = await producManager.createProduct(body)
+  res.status(201).send(product)
+})
+
+app.delete('/api/products/:id', async (req, res) => {
+  const { id } = req.params
+  const productId = parseInt(id)
+
+  if(!await producManager.getProductById(productId)) {
+    res.sendStatus(404)
+    return
+  }
+
+  await producManager.deleteProduct(productId)
+  res.sendStatus(200)
+})
+
+app.put('/api/products/:id', async (req, res) => {
+  const { id } = req.params
+  const productId = parseInt(id)
+  const { body } = req
+
+  try {
+    if(!await producManager.getProductById(productId)){ 
+      res.sendStatus(404)
+      return
+    }
+
+    await producManager.saveProduct(productId, body)
+    res.sendStatus(202)
+
+  } catch(err) {
+    res.status(500).send({
+      message:'Ha ocurrido un error en el servidor',
+      exeption: err.stack
+    })
+  }
+  
 })
 
 const port = 3000
--- src/productManager.js
+++ src/productManager.js
@@ -2,95 +2,81 @@ const fs = require('fs/promises')
 const path = require('path')
 
 class ProductManager {
+  #products = []
   constructor(filename) {
     this.filename = filename
     this.filepath = path.join(__dirname, 'products.json')
-    this.products = []
   }
 
-  async addProduct ({title, description, price, thumbnail, code, stock, id}) {
+  #readFile = async () => {
     const data = await fs.readFile(this.filepath, 'utf-8')
-    this.products = JSON.parse(data) 
+    this.#products = JSON.parse(data)
+  }
 
-    const existingProducts = this.products?.find(product => product.code === code)
-    if(existingProducts) {
-      console.log(`El producto con el ${code} ya esxiste`)
-      return
+  #writeFile = async () => {
+    const data = JSON.stringify(this.#products, null, 2)
+    await fs.writeFile(this.filepath, data)
+  }
+
+  async getProducts(){
+    await this.#readFile() 
+    return this.#products
+  }
+
+  async getProductById(productId) {
+    await this.#readFile()
+    return this.#products?.find(product => product.id === productId)
+  }
+
+  async createProduct(product) {
+    await this.#readFile()
+
+    const id = (this.#products[this.#products.length - 1]?.id || 0) + 1
+
+    const newProduct = {
+      ...product,
+      id
     }
 
-    if(!title || !description || !price || !thumbnail || !code || !stock) {
-      console.log("Todos los campos son obligatorios")
-      return
+    this.#products.push(newProduct)
+    await this.#writeFile()
+
+    return newProduct
+  }
+
+  async saveProduct(id, product){
+    await this.#readFile()
+
+    const existing = await this.getProductById(id)
+
+    if(!existing) {
+      return 
     }
 
-    const newId = this.products[products.length - 1]?.id || 1
-    this.products.push({
+    const {
       title,
       description,
       price,
       thumbnail,
       code,
       stock,
-      id: newId + 1
-    })
+    } = product
 
-    await fs.writeFile(this.filepath, JSON.stringify(products, null, 2))
-    console.log(`El producto ${title} fue agregado`)
-  }
-  
-  async getProducts(){
-    const data = await fs.readFile(this.filepath, 'utf-8')
-     this.products = JSON.parse(data)
+    existing.title = title
+    existing.description = description
+    existing.price = price
+    existing.thumbnail = thumbnail
+    existing.code = code 
+    existing.stock = stock
 
-    return this.products
-  }
-
-  async getProductById(productId) {
-    const data = await fs.readFile(this.filepath, 'utf-8')
-    this.products = JSON.parse(data)
-
-    const findProduct = this.products?.find(product => product.id === productId)
-    
-    if (findProduct) {
-      return findProduct;
-    } else {
-      console.log(`El producto con ID ${productId} no existe`);
-      return null;
-    }
-    // return findProduct ? console.log(findProduct) : console.log(`El producto con ID ${productId} no existe`)
-  }
-
-  async updateProduct(productId, {title, description, price, thumbnail, code, stock}) {
-    const data = await fs.readFile(this.filepath, 'utf-8')
-    this.products = JSON.parse(data)
-
-    const updateProduct = this.products.map(product => {
-      if(product.id === productId) {
-        return {
-          ...product,
-          title: title || product.title,
-          description: description || product.description,
-          price: price || product.price,
-          thumbnail: thumbnail || product.thumbnail,
-          code: code || product.code,
-          stock: stock || product.stock
-        };
-      }
-      return product
-    })
-
-    await fs.writeFile(this.filepath, JSON.stringify(updateProduct, null, 2))
-    console.log(`El producto con ID: ${productId} fue actualizado`)
+    await this.#writeFile()
   }
 
   async deleteProduct(productId) {
-    const data = await fs.readFile(this.filepath, 'utf-8')
-    this.products = JSON.parse(data)
+    await this.#readFile()
+    this.#products =  this.#products?.filter(product => product.id !== productId)
 
-    const updateProducts = this.products?.filter(product => product.id !== productId)
-   
-   await fs.writeFile(this.filepath, JSON.stringify(updateProducts, null, 2)) 
-    console.log(`El producto ID:${productId} fue eliminado`)
+    await this.#writeFile()
   }
 }
  
